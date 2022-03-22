7e8c9066567808a3b1f70851b8356ebe5d3eab25
DIGITAL FUNCS <digitalfuncs@gmail.com>
2022-03-22 14:26:24 +0400

    fix show graph

--- src/App.vue
+++ src/App.vue
@@ -292,6 +292,9 @@ export default {
       this.tickers
         .filter((t) => t.name === tickerName)
         .forEach((t) => {
+          if (t === this.selectedTicker) {
+            this.graph.push(price);
+          }
           t.price = price;
         });
     },
